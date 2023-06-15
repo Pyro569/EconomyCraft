4be232ae6e0f6ad1f11c398e7a11852876e52768
Pyro <79991107+Pyro569@users.noreply.github.com>
2023-06-15 17:19:50 -0400

    Britain Update

--- src/main/java/com/Pyro569/EconomyCraft/item/ModItems.java
+++ src/main/java/com/Pyro569/EconomyCraft/item/ModItems.java
@@ -6,6 +6,7 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import org.lwjgl.system.CallbackI;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, EconomyCraft.MOD_ID);
@@ -19,4 +20,12 @@ public class ModItems {
     public static final RegistryObject<Item> NICKEL = ITEMS.register("nickel", () -> new Item(new Item.Properties().group(ModItemgroup.ECONOMYCRAFT)));
     public static final RegistryObject<Item> DIME = ITEMS.register("dime", () -> new Item(new Item.Properties().group(ModItemgroup.ECONOMYCRAFT)));
     public static final RegistryObject<Item> QUARTER = ITEMS.register("quarter", () -> new Item(new Item.Properties().group(ModItemgroup.ECONOMYCRAFT)));
+    public static final RegistryObject<Item> BRITISHPENNY = ITEMS.register("britishpenny", () -> new Item(new Item.Properties().group(ModItemgroup.ECONOMYCRAFT)));
+    public static final RegistryObject<Item> TWOPENCE = ITEMS.register("twopence", () -> new Item(new Item.Properties().group(ModItemgroup.ECONOMYCRAFT)));
+    public static final RegistryObject<Item> FIVEPENCE = ITEMS.register("fivepence", () -> new Item(new Item.Properties().group(ModItemgroup.ECONOMYCRAFT)));
+    public static final RegistryObject<Item> TENPENCE = ITEMS.register("tenpence", () -> new Item(new Item.Properties().group(ModItemgroup.ECONOMYCRAFT)));
+    public static final RegistryObject<Item> TWENTYPENCE = ITEMS.register("twentypence", () -> new Item(new Item.Properties().group(ModItemgroup.ECONOMYCRAFT)));
+    public static final RegistryObject<Item> FIFTYPENCE = ITEMS.register("fiftypence", () -> new Item(new Item.Properties().group(ModItemgroup.ECONOMYCRAFT)));
+    public static final RegistryObject<Item> POUND = ITEMS.register("pound", () -> new Item(new Item.Properties().group(ModItemgroup.ECONOMYCRAFT)));
+    public static final RegistryObject<Item> TWOPOUND = ITEMS.register("twopound", () -> new Item(new Item.Properties().group(ModItemgroup.ECONOMYCRAFT)));
 }
